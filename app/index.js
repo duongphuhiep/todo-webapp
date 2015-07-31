ea7caa542b62311cb17b1f76029b6a679765e5c2
hiep <duongphuhiep@gmail.com>
2015-07-31 02:14:18 +0200

    add edit button

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+"use strict";
 require("../../bower_components/riot/riot"); //the riot object is global
 require("../bower_components/materialize/dist/css/materialize.css!"); // css for materialize
 require("../gen/tags"); //riot tags
@@ -7,5 +8,9 @@ riot.Model = require("../gen/todo/Model");
 riot.RiotControl = require("../../node_modules/riotcontrol/riotcontrol");
 
 require("../gen/todo/presenter"); //after init Model
+
+riot.route(function(action, itemId) {
+	console.log("edit "+decodeURI(itemId));
+});
 
 riot.tags = riot.mount("*");
